15bfad0fefbea232edbcdf651cd772f7393f1d74
Александр Киндратишин <aleksandrkindratisin@MacBook-Air-Aleksandr.local>
2023-07-10 22:40:25 +0300

    добавлдена карусель в блок ПАРТНЕРЫ

---
+++ js/partners.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.carousel-slide').slick({
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      prevArrow: '<button class="slick-prev" aria-label="Previous" type="button"></button>',
+      nextArrow: '<button class="slick-next" aria-label="Next" type="button"></button>',
+      infinite: true
+    });
+  });
