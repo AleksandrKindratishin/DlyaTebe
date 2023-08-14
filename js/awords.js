8116dad763e2e5926b524de0d3acbe833b872c0b
Aleksandr Kindratishin <kindratishin24kindra@gmail.com>
2023-08-14 12:47:54 +0300

    переделаны некоторые элементы сайта в версии для ПК

--- js/awords.js
+++ js/awords.js
@@ -1,16 +1,15 @@
-const awardsSlider = document.querySelector(".awards__slides");
-const slideWidth = awardsSlider.offsetWidth;
-let currentSlide = 0;
-const numSlides = awardsSlider.children.length;
-
-function moveToSlide(slideIndex) {
-    if (slideIndex < 0 || slideIndex >= numSlides) {
-        return;
-    }
-    currentSlide = slideIndex;
-    awardsSlider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
-}
-
-setInterval(() => {
-    moveToSlide((currentSlide + 1) % numSlides);
-}, 10000);
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper('.awards__slider', {
+        slidesPerView: 3, // Отображаем 3 слайда одновременно
+        spaceBetween: 10, // Отступ между слайдами
+        loop: true, // Зацикливаем карусель
+        loopedSlides: 8, // Количество дополнительных слайдов, чтобы карусель начинала заново
+        autoplay: {
+            delay: 10000, // Прокручивать каждые 10 секунд
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
--- js/projects.js
+++ js/projects.js
@@ -1,11 +1,15 @@
 document.addEventListener("DOMContentLoaded", function () {
     const swiper = new Swiper('.swiper-container', {
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      slidesPerView: 1, // Отображаем один слайд за раз
-      spaceBetween: 20, // Отступ между слайдами
-      loop: true, // Бесконечная прокрутка
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination', // Элемент для точек слайдера
+            clickable: true, // Делаем точки кликабельными
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
     });
-  });
+});
